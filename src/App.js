98b69f557bd014ca6d878b28fb3bb9171cc42665
Iceyuu <93038986+Iceyuu@users.noreply.github.com>
2023-06-03 10:57:51 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar'
+import Content from './components/content/Content'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
